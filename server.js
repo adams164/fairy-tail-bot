17df066cb85123b96f2b67930ceeebbb4be940f8
Glitch (fairy-tail-bot) <none>
2018-10-12 10:14:10 +0000

    Resolve merge conflicts

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-<<<<<<< HEAD
-=======
+
 const cqstats = require("./commands/cqstats.js");
 
 var TOKEN;
@@ -12,7 +11,6 @@ try {
 } catch (err) {
   TOKEN = process.env.TOKEN
 }
->>>>>>> dev-branch
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -32,14 +30,6 @@ client.on("message", (message) => {
 
   const Exp = /^[0-9a-z]+$/
   if (command.match(Exp)) {
-<<<<<<< HEAD
-    try {
-      let commandFile = require(`./commands/${command}.js`);
-      commandFile.run(client, message, args);
-    }
-    catch (err) {
-      message.channel.send("no such command");
-=======
     if (command == "cqstats"){
       cqstats.run(client, message, args);
     }
@@ -52,7 +42,7 @@ client.on("message", (message) => {
         console.log(err)
         message.channel.send("no such command");
       }
->>>>>>> dev-branch
+
     }
   }
 });
