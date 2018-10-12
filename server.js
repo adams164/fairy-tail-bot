95d36c3354bdf684ad05bfc1127cd021b55b6d35
Coletrain <adams164@gmail.com>
2018-10-11 21:50:04 -0500

    Command handler with single command

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
 
 client.on("ready", () => {
   console.log("I am ready!");
 }
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(config.prefix)) {
+    //do something with normal messages
+    return;
+  }
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+  const Exp = /^[0-9a-z]+$/
+  if (command.match(Exp)) {
+    try {
+      let commandFile = require(`./commands/${command}.js`);
+      commandFile.run(client, message, args);
+    }
+    catch (err) {
+      message.channel.send("no such command");
+    }
   }
 });
 
